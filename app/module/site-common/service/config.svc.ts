232e1d091a7430fc2310568ad5e441caa53e8207
dudewad <me@laytonmiller.com>
2017-10-01 23:34:12 +0200

    v1.0.4. Add version to app's global runtime config. Fix breaks caused by router upgrade (more may need to happen but for now app seems stable). Also this fixes ENV pointers that broke the metrics service ('prod' and 'production' are now both valid 'production' environment aliases for webpack configs). App now outputs version number on bootstrap. Minor formatting fixes here and there. Fix example HTaccess policy to actually work. Probably not a good idea to whitelist everything but need to figure out a better functioninig policy at a later time.

--- app/module/site-common/service/config.svc.ts
+++ app/module/site-common/service/config.svc.ts
@@ -1,48 +1,48 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {BehaviorSubject} from 'rxjs';
 
 export const ConfigTypes: any = {
-	app: 'app',
-	global: 'global',
-	page: 'page'
+    app: 'app',
+    global: 'global',
+    page: 'page'
 };
 
 @Injectable()
 export class Config_Svc {
-	configUpdate: BehaviorSubject<any> = new BehaviorSubject<any>({});
-	globalConfigUpdate: BehaviorSubject<any> = new BehaviorSubject<any>({});
-	pageConfigUpdate: BehaviorSubject<any> = new BehaviorSubject<any>({});
-	appConfigUpdate: BehaviorSubject<any> = new BehaviorSubject<any>({});
+    configUpdate: BehaviorSubject<any> = new BehaviorSubject<any>({});
+    globalConfigUpdate: BehaviorSubject<any> = new BehaviorSubject<any>({});
+    pageConfigUpdate: BehaviorSubject<any> = new BehaviorSubject<any>({});
+    appConfigUpdate: BehaviorSubject<any> = new BehaviorSubject<any>({});
 
-	private configs:any = {};
+    private configs: any = {};
 
-	constructor() {
-	}
+    constructor() {
+    }
 
-	setConfig(type, config:any, currentRoute?: any) {
-		let eventData = {
-			type,
-			config,
-			currentRoute
-		};
+    setConfig(type, config: any, currentRoute?: any) {
+        let eventData = {
+            type,
+            config,
+            currentRoute
+        };
 
-		this.configs[type] = config;
-		this.configUpdate.next(eventData);
+        this.configs[type] = config;
+        this.configUpdate.next(eventData);
 
-		switch(type) {
-			case ConfigTypes.app:
-				this.appConfigUpdate.next(eventData);
-				break;
-			case ConfigTypes.global:
-				this.globalConfigUpdate.next(eventData);
-				break;
-			case ConfigTypes.page:
-				this.pageConfigUpdate.next(eventData);
-				break;
-		}
-	}
+        switch (type) {
+            case ConfigTypes.app:
+                this.appConfigUpdate.next(eventData);
+                break;
+            case ConfigTypes.global:
+                this.globalConfigUpdate.next(eventData);
+                break;
+            case ConfigTypes.page:
+                this.pageConfigUpdate.next(eventData);
+                break;
+        }
+    }
 
-	getConfig(type):any {
-		return this.configs[type];
-	}
+    getConfig(type): any {
+        return this.configs[type];
+    }
 }
--- app/module/site-common/service/metrics.svc.ts
+++ app/module/site-common/service/metrics.svc.ts
@@ -1,108 +1,107 @@
-import { Injectable } from '@angular/core';
-
-import { Config_Svc, ConfigTypes } from "./config.svc";
+import {Injectable} from '@angular/core';
+import {Router, NavigationEnd} from '@angular/router';
+import {Subscription} from 'rxjs';
 
 export const MetricTypes: any = {
-	google: 'google'
+    google: 'google'
 };
 
 const MetricScriptUrls: any = {
-	google: 'https://www.google-analytics.com/analytics.js'
+    google: 'https://www.google-analytics.com/analytics.js'
 };
 
 @Injectable()
 export class Metrics_Svc {
-	private currentPageData: any;
-	private metricsConfigs: any;
-	private metricsFired: any;
-	private metricsInstances: any = {};
+    private metricsConfigs: any;
+    private metricsFired: any;
+    private metricsInstances: any = {};
+    private routerEventSub: Subscription;
 
-	constructor(private configSvc: Config_Svc) {
-		if (ENV !== 'production') {
-			console.log('Metrics are disabled when not in production mode.');
-			return;
-		}
+    constructor(private router: Router) {
+        if (ENV !== 'production' && ENV !== 'prod') {
+            console.log('Metrics are disabled when not in production mode.');
+            return;
+        }
 
-		configSvc.pageConfigUpdate
-			.subscribe(data => {
-				this.handlePageLoad(data);
-			});
-	}
+        this.routerEventSub = this.router.events
+            .filter(evt => evt instanceof NavigationEnd)
+            .subscribe(evt => {
+                this.onNavigationEnd(evt);
+            });
+    }
 
-	init(metrics) {
-		if (ENV !== 'production') {
-			return;
-		}
+    init(metrics) {
+        if (ENV !== 'production' && ENV !== 'prod') {
+            return;
+        }
 
-		this.metricsConfigs = metrics;
-		this.initMetrics();
-	}
+        this.metricsConfigs = metrics;
+        this.initMetrics();
+    }
 
-	private handlePageLoad(data: any) {
-		let instances = this.metricsInstances;
+    private onNavigationEnd(evt: any) {
+        let instances = this.metricsInstances;
 
-		this.metricsFired = {};
-		this.currentPageData = data;
+        this.metricsFired = {};
 
-		for (let type in instances) {
-			if (instances.hasOwnProperty(type)) {
-				this.firePageLoad(type);
-			}
-		}
-	}
+        for (let type in instances) {
+            if (instances.hasOwnProperty(type)) {
+                this.firePageLoad(type);
+            }
+        }
+    }
 
-	private firePageLoad(type: string) {
-		if (!this.metricsFired || this.metricsFired[type]) {
-			return;
-		}
+    private firePageLoad(type: string) {
+        if (!this.metricsFired || this.metricsFired[type]) {
+            return;
+        }
 
-		let instances = this.metricsInstances;
-		let data = this.currentPageData;
-		this.metricsFired[type] = true;
+        let instances = this.metricsInstances;
+        this.metricsFired[type] = true;
 
-		switch (type) {
-			case MetricTypes.google:
-				let ga = instances[type];
-				ga('set', 'pageview', data.currentRoute.url);
-				ga('send', 'pageview');
-				break;
-		}
-	}
+        switch (type) {
+            case MetricTypes.google:
+                let ga = instances[type];
+                ga('set', 'pageview', this.router.routerState.snapshot.url);
+                ga('send', 'pageview');
+                break;
+        }
+    }
 
-	private initMetrics() {
-		let instances = this.metricsInstances;
-		let cfgs = this.metricsConfigs;
+    private initMetrics() {
+        let instances = this.metricsInstances;
+        let cfgs = this.metricsConfigs;
 
-		for (let cfg in cfgs) {
-			if (cfgs.hasOwnProperty(cfg) && !instances[cfg]) {
-				switch (cfg) {
-					case MetricTypes.google:
-						this.loadGoogleAnalytics();
-						break;
-				}
-			}
-		}
-	}
+        for (let cfg in cfgs) {
+            if (cfgs.hasOwnProperty(cfg) && !instances[cfg]) {
+                switch (cfg) {
+                    case MetricTypes.google:
+                        this.loadGoogleAnalytics();
+                        break;
+                }
+            }
+        }
+    }
 
-	private loadGoogleAnalytics() {
-		let cb = this.onGoogleLoad.bind(this);
-		(function (i, s, o, g, r, a, m) {
-			i['GoogleAnalyticsObject'] = r;
-			i[r] = i[r] || function () {
-					(i[r].q = i[r].q || []).push(arguments)
-				}, i[r].l = 1 * <any>(new Date());
-			a = s.createElement(o),
-				m = s.getElementsByTagName(o)[0];
-			a.async = 1;
-			a.src = g;
-			a.onload = cb;
-			m.parentNode.insertBefore(a, m)
-		})(window, document, 'script', MetricScriptUrls.google, 'ga');
-	}
+    private loadGoogleAnalytics() {
+        let cb = this.onGoogleLoad.bind(this);
+        (function (i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r;
+            i[r] = i[r] || function () {
+                (i[r].q = i[r].q || []).push(arguments)
+            }, i[r].l = 1 * <any>(new Date());
+            a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            a.onload = cb;
+            m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', MetricScriptUrls.google, 'ga');
+    }
 
-	private onGoogleLoad() {
-		this.metricsInstances[MetricTypes.google] = ga;
-		ga('create', this.metricsConfigs.google.trackingId, 'auto');
-		this.firePageLoad(MetricTypes.google);
-	}
+    private onGoogleLoad() {
+        this.metricsInstances[MetricTypes.google] = ga;
+        ga('create', this.metricsConfigs.google.trackingId, 'auto');
+        this.firePageLoad(MetricTypes.google);
+    }
 }
