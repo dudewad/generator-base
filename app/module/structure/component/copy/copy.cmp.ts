0a75931b8e3d20434afe3f12f9bba87f7cbceab3
dudewad <me@laytonmiller.com>
2018-04-01 18:23:55 -0700

    Fix global-event service resize bug by running resize handlers outside the ngZone

--- app/module/structure/component/copy/copy.cmp.ts
+++ app/module/structure/component/copy/copy.cmp.ts
@@ -1,20 +1,25 @@
 import { Component, Inject } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 
-import { App_Const, Asset_Svc, GlobalEvent_Svc, Localization_Svc } from 'lm/site-common';
+import {
+    App_Const,
+    Asset_Svc,
+    GlobalEvent_Svc,
+    Localization_Svc
+} from 'lm/site-common';
 import { StructureBase_Cmp } from 'lm/structure';
 
 @Component({
-	selector: 'copy',
-	template: require('./copy.cmp.html'),
-	styles: [require('./copy.cmp.scss')]
+    selector: 'copy',
+    template: require('./copy.cmp.html'),
+    styles: [require('./copy.cmp.scss')]
 })
 export class Copy_Cmp extends StructureBase_Cmp {
-	constructor(protected sanitizer: DomSanitizer,
-	            @Inject(App_Const) protected constants,
-	            protected assetSvc: Asset_Svc,
-	            protected globalEventSvc: GlobalEvent_Svc,
-	            protected locSvc: Localization_Svc) {
-		super(sanitizer, constants, assetSvc, globalEventSvc, locSvc);
-	}
+    constructor(protected sanitizer: DomSanitizer,
+                @Inject(App_Const) protected constants,
+                protected assetSvc: Asset_Svc,
+                protected globalEventSvc: GlobalEvent_Svc,
+                protected locSvc: Localization_Svc) {
+        super(sanitizer, constants, assetSvc, globalEventSvc, locSvc);
+    }
 }
